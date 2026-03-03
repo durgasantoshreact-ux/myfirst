25afdddcce3b7ff05df038f4d3c8c0ef8d3b4ef3
Santosh <durgasantosh6@gmail.com>
2026-03-03 14:33:27 +0530

    latest changes

--- src/home.js
+++ src/home.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+let regain=['angular','react','vue','svelte'];
+
+
 function Home(){
+   let [name1,updatename1]= useState('santosh');
+   useEffect(()=>{
+     updatename1('Lakshman')
+   },[])
    return(
-      <div>Home Page</div>
+      // {regain.map((val) => {
+      //     return <h1>{val}  </h1>
+     <div> 
+      <p>{name1}</p>
+    {
+      regain.map((val) => {
+          return <h1>{val}  </h1>
+       })
+    }
+      </div>
    )
 }
 export default Home;
